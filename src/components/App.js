8b76c8848357a76249a7fba462c91981a9abcff7
Blade <bladeprojectz@gmail.com>
2024-04-02 06:22:29 +0800

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [image, setImage] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((img) => setImage(img.message));
+
+    setIsLoaded(true);
+  }, []);
+
+  return isLoaded ? <img src={image} alt="A Random Dog" /> : <p>Loading...</p>;
+}
+
+export default App;
